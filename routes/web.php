21609ad3798d1b08e6613ed04e8a3ac84c299ddf
dzaky santino <88192405+dzaky-santino@users.noreply.github.com>
2025-02-17 12:08:59 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
 
 Route::get('/', function () {
     return view('home');
@@ -10,6 +11,13 @@ Route::get('recommendation-engine', function () {
     return view('recommendation-engine');
 })->name('recommendation');
 
+Route::get('fraud-detection', function () {
+    return view('fraud-detection');
+})->name('fraud');
+
 Route::get('form', function () {
-    return view('form');
+    return view('personal-data.form');
 })->name('form');
+
+Route::get('/personal-data', [FormController::class, 'index'])->name('personal-data.index');
+Route::post('/personal-data', [FormController::class, 'store'])->name('personal-data.store');
